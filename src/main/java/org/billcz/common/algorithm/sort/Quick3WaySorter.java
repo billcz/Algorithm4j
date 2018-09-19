4eb8df0f420ae84713795098d72ad3a737c63c20
billcz <hk_whu_bill@hotmail.com>
2018-09-19 19:56:19 +0800

    Add Quick3WaySorter

--- src/main/java/org/billcz/common/algorithm/sort/Quick3WaySorter.java
+++ src/main/java/org/billcz/common/algorithm/sort/Quick3WaySorter.java
@@ -2,6 +2,8 @@ package org.billcz.common.algorithm.sort;
 
 import org.billcz.common.algorithm.util.Arrays;
 
+import java.util.Comparator;
+
 /**
  * Description:
  * Author: billcz
@@ -12,6 +14,12 @@ public class Quick3WaySorter extends AbstractSorter {
         Arrays.shuffle(values);
         sort(values, 0, values.length - 1);
     }
+
+    public static void sort(Object[] values, Comparator comparator) {
+        Arrays.shuffle(values);
+        sort(values, 0, values.length - 1, comparator);
+    }
+
 
     private static void sort(Comparable[] values, int lo, int hi) {
         if (lo >= hi) return;
@@ -35,4 +43,30 @@ public class Quick3WaySorter extends AbstractSorter {
         sort(values, lo, lt - 1);
         sort(values, gt + 1, hi);
     }
+
+
+
+    private static void sort(Object[] values, int lo, int hi, Comparator comparator) {
+        if (lo >= hi) return;
+
+        int lt = lo;
+        int gt = hi;
+        Object v = values[lo];
+        int i = lo + 1;
+
+        while (i <= gt) {
+            int cmp = comparator.compare(values[i], v);
+            if (cmp < 0) {
+                exchange(values, lt++, i++);
+            } else if (cmp > 0) {
+                exchange(values, i, gt--);
+            } else {
+                i++;
+            }
+        }
+
+        sort(values, lo, lt - 1, comparator);
+        sort(values, gt + 1, hi, comparator);
+    }
+
 }
