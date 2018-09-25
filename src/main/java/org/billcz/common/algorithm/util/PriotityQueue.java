af679de1d9a5f09c1b253b64084e928dd7108a73
billcz <hk_whu_bill@hotmail.com>
2018-09-25 20:02:55 +0800

    Add PriorityQueue(topN)

---
+++ src/main/java/org/billcz/common/algorithm/util/PriotityQueue.java
@@ -0,0 +1,94 @@
+package org.billcz.common.algorithm.util;
+
+import java.util.Comparator;
+
+/**
+ * Description:
+ * Author: billcz
+ * Create time: 2018/9/19
+ */
+public class PriotityQueue<Item> {
+    private Item[] queues;
+    private int topN;
+    private Comparator comparator;
+    private int size;
+
+    public PriotityQueue(int topN, Comparator<Item> comparator) {
+        this.topN = topN;
+        this.comparator = comparator;
+        this.queues = (Item[]) new Object[topN];
+    }
+
+    public void add(Item item) {
+        if (size == 0) {
+            queues[size++] = item;
+            return;
+        }
+
+        if (size < topN) {
+            int i = size;
+            siftUp(i, item);
+            size++;
+        } else {
+            Item first = peek();
+            if (comparator.compare(item, first) <= 0) return;
+            poll();
+            add(item);
+        }
+
+
+    }
+
+    public Item peek() {
+        return queues[0];
+    }
+
+    public Item poll() {
+        Item result = queues[0];
+        Item x = queues[size - 1];
+        siftDown(0, x);
+        size--;
+        return result;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private void siftUp(int k, Item item) {
+        while (k > 0) {
+            int parrent = (k - 1) >>> 1;
+            Item p = queues[parrent];
+            if (comparator.compare(item, p) >= 0)
+                break;
+            queues[k] = p;
+            k = parrent;
+        }
+
+        queues[k] = item;
+    }
+
+    private void siftDown(int k, Item item) {
+        int half = size >>> 1;
+        while (k < half) {
+            int child = (k << 1)  + 1;
+            Item c = queues[child];
+            int right = child + 1;
+            if (right < size && comparator.compare(c, queues[right]) > 0) {
+                child = right;
+                c = queues[child];
+            }
+
+            if (comparator.compare(item, c) <= 0)
+                break;
+            queues[k] = c;
+            k = child;
+        }
+
+        queues[k] = item;
+
+    }
+
+
+
+}
---
+++ src/test/java/org/billcz/common/util/PriorityQueueTest.java
@@ -0,0 +1,32 @@
+package org.billcz.common.util;
+
+
+import org.billcz.common.algorithm.util.PriotityQueue;
+
+import java.util.Comparator;
+
+/**
+ * Description:
+ * Author: billcz
+ * Create time: 2018/9/25
+ */
+public class PriorityQueueTest {
+    public static void main(String[] args) {
+        Integer[] values = new Integer[] { 3, 1, 3, 2, 5, 3, 5, 4, 9, 6, 7 , 8, 0 };
+        PriotityQueue<Integer> pq = new PriotityQueue<Integer>(3, new Comparator<Integer>() {
+            public int compare(Integer o1, Integer o2) {
+                return o1 - o2;
+            }
+        });
+
+        for (Integer i : values) {
+            pq.add(i);
+        }
+
+        while (!pq.isEmpty()) {
+            System.out.println(pq.poll());
+        }
+
+
+    }
+}
