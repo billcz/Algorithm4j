f4e573fe6feec8fdc268eb017c8421cb7a81515b
billcz <hk_whu_bill@hotmail.com>
2018-09-26 19:44:50 +0800

    Add Search(Dict)

--- src/main/java/org/billcz/common/algorithm/search/HashDict.java
+++ src/main/java/org/billcz/common/algorithm/search/HashDict.java
@@ -2,19 +2,27 @@ package org.billcz.common.algorithm.search;
 
 import org.billcz.common.algorithm.util.Set;
 
+import java.util.HashMap;
+
 /**
  * Description:
  * Author: billcz
  * Create time: 2018/9/26
  */
 public class HashDict<K, V> implements Dict<K, V> {
+    private Node[] tables;
+    private int size;
 
     public void put(K k, V v) {
-
     }
 
     public V get(K k) {
-        return null;
+        int hashCode = hash(k);
+        Node root = tables[hashCode];
+        V result = null;
+        while (root.next != null) {
+        }
+        return result;
     }
 
     public void delete(K k) {
@@ -22,14 +30,24 @@ public class HashDict<K, V> implements Dict<K, V> {
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     public Set<K> Keys() {
         return null;
+    }
+
+    private class Node {
+        private K k;
+        private V v;
+        private Node next;
+    }
+
+    public int hash(Object o) {
+        return o == null ? 0 : o.hashCode();
     }
 }
