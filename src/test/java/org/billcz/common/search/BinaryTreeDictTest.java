144e1840c098871b20dedca3ac5d67a90d5a4e73
billcz <hk_whu_bill@hotmail.com>
2018-09-27 16:47:23 +0800

    Add Search(Dict)

---
+++ src/test/java/org/billcz/common/search/BinaryTreeDictTest.java
@@ -0,0 +1,24 @@
+package org.billcz.common.search;
+
+import org.billcz.common.algorithm.search.BinarySearchDict;
+import org.billcz.common.algorithm.search.BinaryTreeDict;
+import org.billcz.common.algorithm.search.SortDict;
+
+/**
+ * Description:
+ * Author: billcz
+ * Create time: 2018/9/26
+ */
+public class BinaryTreeDictTest {
+    public static void main(String[] args) {
+        SortDict<Integer, String> sortDict = new BinaryTreeDict<Integer, String>();
+
+        for (int i = 0; i < 100; i++) {
+            sortDict.put(i, "i" + i);
+        }
+
+        System.out.println(sortDict.getFirst());
+        System.out.println(sortDict.getLast());
+
+    }
+}
