98fcdba704cac83348b37fc43dddf3de359551a0
billcz <hk_whu_bill@hotmail.com>
2018-10-15 20:06:23 +0800

    Add Graph data

--- src/test/java/org/billcz/common/graph/GraphTest.java
+++ src/test/java/org/billcz/common/graph/GraphTest.java
@@ -8,14 +8,32 @@ import org.billcz.common.algorithm.graph.Graph;
  * Create time: 2018/10/15
  */
 public class GraphTest {
-    public static void main(String[] args) {
-        Graph graph = new Graph(5);
 
-        for (int v = 0; v < 5; v++) {
-            for (int w = 1; w < 5; w++) {
-                graph.addEdge(v, w);
-            }
+
+    public static void main(String[] args) {
+        String tinyG = "0 5\n" +
+                "4 3\n" +
+                "0 1\n" +
+                "9 12\n" +
+                "6 4\n" +
+                "5 4\n" +
+                "0 2\n" +
+                "11 12\n" +
+                "9 10\n" +
+                "0 6\n" +
+                "7 8\n" +
+                "9 11\n" +
+                "5 3";
+
+        Graph graph = new Graph(13);
+        String[] fields = tinyG.split("\n");
+        for (String field : fields) {
+            String[] vals = field.split(" ");
+            int v = Integer.parseInt(vals[0]);
+            int w= Integer.parseInt(vals[1]);
+            graph.addEdge(v, w);
         }
+
         System.out.println(graph.toString());
 
         Graph copy = new Graph(graph);
