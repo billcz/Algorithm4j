a69f5fb17c7ba9a1a09c50fca1a75cbc93b6e364
billcz <hk_whu_bill@hotmail.com>
2018-10-17 19:29:49 +0800

    Add DiGraph

---
+++ src/main/java/org/billcz/common/algorithm/graph/SymbolGraph.java
@@ -0,0 +1,20 @@
+package org.billcz.common.algorithm.graph;
+
+import org.billcz.common.algorithm.search.Dict;
+
+/**
+ * Description:
+ * Author: billcz
+ * Create time: 2018/10/17
+ */
+public class SymbolGraph {
+    private Dict<String, Integer> st;
+    private String[] keys;
+    private Graph graph;
+
+
+
+
+
+
+}
