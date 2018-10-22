f055e4540e2e76b662e434b660cbe3e0c9d1c8ec
billcz <hk_whu_bill@hotmail.com>
2018-10-22 23:12:42 +0800

    Add SCC

---
+++ src/main/java/org/billcz/common/algorithm/graph/SCC.java
@@ -0,0 +1,24 @@
+package org.billcz.common.algorithm.graph;
+
+/**
+ * Description:
+ * Author: billcz
+ * Create time: 22/10/2018
+ */
+public class SCC {
+    public SCC(DiGraph diGraph) {
+
+    }
+
+    public boolean stronglyConnected(int v, int w) {
+        return false;
+    }
+
+    public int count() {
+        return 0;
+    }
+
+    public int id(int v) {
+        return 0;
+    }
+}
