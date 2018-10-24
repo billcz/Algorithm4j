e776bdbcfd8495e1f4ab9ea8fe4307ff1386599d
billcz <hk_whu_bill@hotmail.com>
2018-10-24 09:34:50 +0800

    Add DiGraph SCC

--- src/main/java/org/billcz/common/algorithm/graph/SCC.java
+++ src/main/java/org/billcz/common/algorithm/graph/SCC.java
@@ -6,19 +6,43 @@ package org.billcz.common.algorithm.graph;
  * Create time: 22/10/2018
  */
 public class SCC {
-    public SCC(DiGraph diGraph) {
+    private boolean[] marked;
+    private int[] ids;
+    private int count;
 
+    public SCC(DiGraph diGraph) {
+        this.marked = new boolean[diGraph.getV()];
+        this.ids = new int[diGraph.getV()];
+        this.count = 0;
+
+        DepthFirstOrder dfOrder = new DepthFirstOrder(diGraph.reverse());
+
+        for (int s : dfOrder.reversePost())
+            if (!marked[s]) {
+                dfs(diGraph, s);
+                count++;
+            }
+    }
+
+    private void dfs(DiGraph diGraph, int v) {
+        marked[v] = true;
+        ids[v] = count;
+
+        for (int w : diGraph.adjs(v)) {
+            if (!marked[w])
+                dfs(diGraph, w);
+        }
     }
 
     public boolean stronglyConnected(int v, int w) {
-        return false;
+        return ids[v] == ids[w];
     }
 
     public int count() {
-        return 0;
+        return count;
     }
 
     public int id(int v) {
-        return 0;
+        return ids[v];
     }
 }
