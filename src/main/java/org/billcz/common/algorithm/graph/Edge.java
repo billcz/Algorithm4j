2919aaaf2c2567ab387f34095fef75acb4d6a8ff
billcz <hk_whu_bill@hotmail.com>
2018-10-24 20:36:21 +0800

    Add EdgeWeightedGraph

---
+++ src/main/java/org/billcz/common/algorithm/graph/Edge.java
@@ -0,0 +1,37 @@
+package org.billcz.common.algorithm.graph;
+
+/**
+ * Description:
+ * Author: billcz
+ * Create time: 2018/10/24
+ */
+public class Edge implements Comparable<Edge> {
+    private int v;
+    private int w;
+    private double weight;
+
+    public Edge(int v, int w, double weight) {
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+    }
+
+    public double weight() {
+        return weight;
+    }
+
+    public int eighter() {
+        return v;
+    }
+
+    public int other(int vertex) {
+        if (vertex == v) return w;
+        if (vertex == w) return v;
+        return -1;
+    }
+
+    public int compareTo(Edge o) {
+        return Double.compare(this.weight, o.weight);
+    }
+
+}
---
+++ src/main/java/org/billcz/common/algorithm/graph/EdgeWeightedGraph.java
@@ -0,0 +1,49 @@
+package org.billcz.common.algorithm.graph;
+
+import org.billcz.common.algorithm.util.Bag;
+import org.billcz.common.algorithm.util.LinkedListBag;
+
+/**
+ * Description:
+ * Author: billcz
+ * Create time: 2018/10/24
+ */
+public class EdgeWeightedGraph {
+    private boolean[] marked;
+    private int V;
+    private int E;
+    private Bag<Edge>[] adjs;
+
+    public EdgeWeightedGraph(int v) {
+        this.V = v;
+        this.E = 0;
+        this.marked = new boolean[v];
+        this.adjs = new LinkedListBag[v];
+
+        for (int i = 0; i < v; i++)
+            this.adjs[i] = new LinkedListBag<Edge>();
+
+    }
+
+    public void addEdge(Edge edge) {
+        int v = edge.eighter();
+        int w = edge.other(v);
+
+        adjs[v].add(edge);
+        adjs[w].add(edge);
+
+        E++;
+    }
+
+    public int getV() {
+        return V;
+    }
+
+    public int getE() {
+        return E;
+    }
+
+    public Iterable<Edge> adjs(int v) {
+        return adjs[v];
+    }
+}
